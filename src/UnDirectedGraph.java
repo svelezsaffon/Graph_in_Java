0feb7ec4ea59caa768c272dd5176c915bdc6e2d3
santiago <svelezsa@purdue.edu>
2015-01-31 20:52:38 -0500

    BFS completed

---
+++ src/UnDirectedGraph.java
@@ -0,0 +1,21 @@
+/**
+ * Created by santiago on 1/31/15.
+ */
+public class UnDirectedGraph extends Graph {
+
+    @Override
+    public boolean insertEdge(Object fromp, Object top, Object valuep) {
+
+        return this.putSingleEdge(fromp,top,valuep)  && this.putSingleEdge(top,fromp,valuep);
+
+
+    }
+
+    @Override
+    public boolean removeEdge(Object from, Object to) {
+        return false;
+    }
+
+
+
+}
